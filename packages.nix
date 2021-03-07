c7890cfef36e2966174b33eeba8c096a0dc6d464
Wadii Hajji <wadii@cardiologs.com>
2021-03-07 22:13:45 +0100

    shell: make shell work somewhat

--- packages.nix
+++ packages.nix
@@ -8,17 +8,23 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wget sxhkd sway wlroots wofi
-    thunderbird firefox-devedition-bin
-    zsh docker git youtube-dl fd
-    brightnessctl ripgrep fzf bspwm zoxide exa
-    docker-compose coreutils redshift-wlr geoclue2
-    spotify xfce.thunar neovim zathura playerctl
+    wget sxhkd sway wlroots wofi thunderbird firefox-devedition-bin zsh docker
+    git youtube-dl fd brightnessctl ripgrep fzf bspwm zoxide exa docker-compose
+    coreutils redshift-wlr geoclue2 spotify xfce.thunar neovim zathura
+    playerctl tmux rustup wlsunset bat
   ];
 
   programs.zsh = {
     enable = true;
+    histSize = 999999;
+    histFile = "/home/wadii/.zsh_history";
     shellAliases = shell.alias;
+    interactiveShellInit = ''
+    ${shell.functions}
+
+    ${shell.misc}
+    '';
+    setOptions = shell.options;
   };
 
   environment.variables = shell.var;
@@ -34,6 +40,12 @@ in
       wofi
       alacritty
       imv
+      grim
+      slurp
+      swaybg
+      wf-recorder
+      xdg-desktop-portal-wlr
+
       xwayland
     ];
   };
@@ -45,13 +57,4 @@ in
   };
 
   services.openssh.enable = true;
-
-  location = {
-    latitude = 48.8;
-    longitude = 2.3;
-  };
-  services.redshift = {
-    enable = true;
-    package = pkgs.redshift-wlr;
-  };
 }
