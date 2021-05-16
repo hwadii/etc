c21b94d259ecdfc138299e837e09ec8489a452e1
Wadii Hajji <wadii@cardiologs.com>
2021-05-16 02:36:10 +0200

    fixup! modules/fzf: add fzf config

--- modules/tools/fzf.nix
+++ modules/tools/fzf.nix
@@ -4,7 +4,12 @@
     programs.fzf = rec {
       enable = true;
       defaultCommand = "${pkgs.fd}/bin/fd --type f --follow --hidden -E'.git'";
-      defaultOptions = [ "--height 40%" "--bind 'tab:down'" "--bind 'btab:up'" "--bind 'ctrl-s:toggle'" ];
+      defaultOptions = [
+        "--height 40%"
+        "--bind 'tab:down' --bind 'btab:up' --bind 'ctrl-s:toggle'"
+        "--bind 'alt-a:select-all'"
+        "--color=16,hl:#ffcb6b,hl+:#89ddff,fg+:7,pointer:#89ddff"
+      ];
       fileWidgetCommand = defaultCommand;
       fileWidgetOptions = [ "--select-1" "--exit-0" "--preview 'head {}'" ];
       changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d --hidden --follow --exclude '.git' --exclude 'node_modules'";
